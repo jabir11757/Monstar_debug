b0d1d86c542ddb7dfcb7b823bab838bbc68e7a9d
jabir11757 <smjalsaba11757@gmail.com>
2022-09-15 09:15:30 +0600

    Start button working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("display");
 const question = document.getElementById("question");
-const startBtn = document.getElementById("start");
+const startBtn = document.getElementById("starts");
 const countdownOverlay = document.getElementById("countdown");
 const resultModal = document.getElementById("result");
 const modalBackground = document.getElementById("modal-background");
@@ -101,13 +101,17 @@ const closeModal = () => {
 
 const start = () => {
   // If already started, do not start again
-  if (startTime) return;
+  if (startTime)
+    //{
+    return;
+  //}
+  //changed
 
   let count = 3;
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
@@ -118,6 +122,8 @@ const start = () => {
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
+
+
     }
     count--;
   }, 1000);
